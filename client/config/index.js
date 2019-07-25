abf3761fd988fe25a866b4ce11716444d07e1bf2
sionelt <sltafengatoto@gmail.com>
2019-07-24 18:20:43 -0600

    update client config with script to google sign in

--- client/config/index.js
+++ client/config/index.js
@@ -2,5 +2,8 @@ const nodeEnv = process.env.NODE_ENV
 
 export default {
   nodeEnv,
-  restApiEndpoint: nodeEnv === 'development' ? process.env.VUE_APP_LOCAL_REST_API_ENDPOINT : process.env.VUE_APP_REST_API_ENDPOINT
+  restApiEndpoint: nodeEnv === 'development' ? process.env.VUE_APP_LOCAL_REST_API_ENDPOINT : process.env.VUE_APP_REST_API_ENDPOINT,
+  google: {
+    clientId: process.env.VUE_APP_GOOGLE_SIGN_IN_CLIENT_ID
+  }
 }
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import bootstrapVue from 'bootstrap-vue'
+
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/services/googleApi.service'
+
+Vue.use(bootstrapVue)
 
 Vue.config.productionTip = false
 
